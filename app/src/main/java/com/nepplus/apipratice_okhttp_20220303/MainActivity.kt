a20e8d6b6d5c7ebad6eae03c48223df76b8bfe2c
YYJ <naki5087@naver.com>
2022-03-03 16:20:41 +0900

    binging변수 이용 -> 이벤트처리 진행 - 실제 API서버 연동 예정

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/MainActivity.kt
@@ -2,14 +2,27 @@ package com.nepplus.apipratice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.apipratice_okhttp_20220303.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+        setupEvents()
+        setValues()
+    }
+    fun setupEvents(){
+        binding.btnLogin.setOnClickListener {
+
+            val inputId = binding.edtId.text.toString()
+            val inputPw = binding.edtPassword.text.toString()
+//            API서버에 아이디/ 비번을 보내서 실제로 회원인지 검사 -> 로그인 시도
+        }
+    }
+    fun setValues(){
 
     }
 }

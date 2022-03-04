0d798937d3fc0502a8631677fbadac545bd31a77
YYJ <naki5087@naver.com>
2022-03-04 15:03:38 +0900

    코틀린단에서도 바인딩 변수 설정

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/SignUpActivity.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/SignUpActivity.kt
@@ -2,10 +2,13 @@ package com.nepplus.apipratice_okhttp_20220303
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.apipratice_okhttp_20220303.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
     }
 }

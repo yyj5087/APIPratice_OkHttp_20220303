b5613c6c3001b70001e9bde4be7c9ddca8da23c8
YYJ <naki5087@naver.com>
2022-03-04 10:35:45 +0900

    실제 리퀘스트 호출 진행

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/utils/ServerUtil.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/utils/ServerUtil.kt
@@ -1,17 +1,18 @@
 package com.nepplus.apipratice_okhttp_20220303.utils
 
-import okhttp3.FormBody
-import okhttp3.Request
+import okhttp3.*
+import java.io.IOException
 
 class ServerUtil {
     //    서버에 Request를 날리는 역할.
 //    함수를 만들려고 하는데, 어던 객체가 실행해도 결과만 잘 나오면 그만인 함수.
 //    코틀린에서 static에 해당하는 개념? companion object { }에 만들자.
     companion object {
-//        서버 컴퓨터 주소만 변수로 저장 (관리 일원화) => 외부 노출 X
+        //        서버 컴퓨터 주소만 변수로 저장 (관리 일원화) => 외부 노출 X
         private val BASE_URL = "http://54.180.52.26"
-//        로그인 기능 호출 함수
-        fun postRequestLogin(id: String, pw: String){
+
+        //        로그인 기능 호출 함수
+        fun postRequestLogin(id: String, pw: String) {
 //            Request 제작 -> 실제 호출 -> 서버의 응답을, 화면에 전달
 
 //            제작 1) 어느 주소 (url) 로 접근할지? => 서버주소 + 기능 주소
@@ -26,6 +27,22 @@ class ServerUtil {
                 .url(urlString)
                 .post(formData)
                 .build()
+//              종합한 Request도 실제 호출을 해 줘야 API 호출이 실행됨. (startActivity 같은 동작 필요함)
+//              실제 호출 : 앱이 클라이언트로써 동작 > OkHttpClient 클래스
+            val client = OkHttpClient()
+//            OkHttpClient 객체를 이용 > 서버에 로그인 기능 실제 호출
+//            호출을 햇으면, 서버가 수행한 결과를 받아서 처리.
+//            => 서버에 다녀와서 할 일을 등록 : enqueue (Callback)
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+
+                }
+
+            })
         }
     }
 }

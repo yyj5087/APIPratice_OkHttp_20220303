c26620cf059c13f6943e9217b4664a2435f3060b
YYJ <naki5087@naver.com>
2022-03-07 15:58:27 +0900

    메인화면 정보 로드 API 기능만 추가

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/utils/ServerUtil.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/utils/ServerUtil.kt
@@ -181,6 +181,33 @@ class ServerUtil {
             })
 
         }
+
+        fun getRequestMainInfo(context: Context, handler: JsonResponseHandler?){
+            val urlBuilder = "${BASE_URL}v2/main_info".toHttpUrlOrNull()!!.newBuilder()
+                .build() // 쿼리파라미터를 담을게 없다. 바로 build로 마무리
+            val urlString = urlBuilder.toString()
+
+            val request = Request.Builder()
+                .url(urlString)
+                .get()
+                .header("X-Http-Token", ContextUtil.getToken(context)) //ContextUtil을 통해, 저장된 토큰을 받아서 첨부.
+                .build()
+
+            val client = OkHttpClient()
+            client.newCall(request).enqueue(object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val jsonObj = JSONObject(response.body!!.string())
+                    Log.d("서버응담", jsonObj.toString())
+                    handler?.onResponse(jsonObj)
+                }
+
+            })
+
+        }
     }
 }
 

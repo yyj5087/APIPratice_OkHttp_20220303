7bedb2433af6f5f2033370f6cc9f3c8f071a8f7a
YYJ <naki5087@naver.com>
2022-03-07 15:47:24 +0900

    연습문제 마무리  - 토큰을 첨부해서, 토큰에 맞는 내 정보를 받아오기

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.databinding.DataBindingUtil
 import com.nepplus.apipratice_okhttp_20220303.databinding.ActivityLoginBinding
 import com.nepplus.apipratice_okhttp_20220303.databinding.ActivityMainBinding
 import com.nepplus.apipratice_okhttp_20220303.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BasicActivity() {
     lateinit var binding: ActivityMainBinding
@@ -23,7 +24,19 @@ class MainActivity : BasicActivity() {
     override fun setValues() {
 
 //        화면의 텍스트뷰에 닉네임을 보여주기 위한 작업
-        //ServerUtil.get
+        ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(jsonObject: JSONObject) {
+
+                val dataObj = jsonObject.getJSONObject("date")
+                val userObj = jsonObject.getJSONObject("user")
+                val nickname = userObj.getString("nick_name")
+
+                runOnUiThread {
+                    binding.txtLoginUserNickname.text = nickname
+                }
+            }
+
+        })
 
     }
 

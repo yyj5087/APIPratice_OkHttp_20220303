bd82b75c9072ed7482cad1ac3dba070d66096e2a
YYJ <naki5087@naver.com>
2022-03-07 16:55:37 +0900

    topics라는 JSONArray 추출

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/MainActivity.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/MainActivity.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.nepplus.apipratice_okhttp_20220303.databinding.ActivityLoginBinding
 import com.nepplus.apipratice_okhttp_20220303.databinding.ActivityMainBinding
+import com.nepplus.apipratice_okhttp_20220303.datas.TopicData
 import com.nepplus.apipratice_okhttp_20220303.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BasicActivity() {
     lateinit var binding: ActivityMainBinding
+    
+//    실제로 서버가 내려주는 주제 목록을 담을 그릇
+    val mTopicList = ArrayList<TopicData>()
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
@@ -23,22 +28,24 @@ class MainActivity : BasicActivity() {
 
     override fun setValues() {
 
-//        화면의 텍스트뷰에 닉네임을 보여주기 위한 작업
-        ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler{
-            override fun onResponse(jsonObject: JSONObject) {
-
-                val dataObj = jsonObject.getJSONObject("date")
-                val userObj = jsonObject.getJSONObject("user")
-                val nickname = userObj.getString("nick_name")
-
-                runOnUiThread {
-                    binding.txtLoginUserNickname.text = nickname
-                }
-            }
-
-        })
+//      메인 화면 정보 가져오기 => API 호출 / 응답 처리
+        getTopicListFromServer()
 
     }
 
+    fun getTopicListFromServer(){
+        ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(jsonObject: JSONObject) {
+                
+//                서버가 주는 토론 주제 목록 파실 => mTopList에 추가하기.
+                val dataObj = jsonObject.getJSONObject("data")
+                val topicsArr = dataObj.getJSONArray("topics")
+                
+//                topicsArr 내부를 하나씩 추출 (JSONObject { }) => TopicData()로 변환
+                
+            }
+
+        })
+    }
 
 }

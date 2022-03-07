f5500493b9c6a1d0cba3c1d2de80847d78bab5bb
YYJ <naki5087@naver.com>
2022-03-07 11:15:30 +0900

    닉네임 중복검사 기능 구현  - 이미 만들어져 있는 함수 재활용.

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/SignUpActivity.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/SignUpActivity.kt
@@ -11,23 +11,46 @@ import com.nepplus.apipratice_okhttp_20220303.utils.ServerUtil
 import org.json.JSONObject
 
 class SignUpActivity : BasicActivity() {
-    lateinit var binding : ActivitySignUpBinding
+    lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
         setupEvents()
         setValues()
 
     }
+
     override fun setupEvents() {
 
-        binding.edtEmail.addTextChangedListener {
 
-//            내용이 한글자라도 바뀌면, 무조건 재검사 요구 문장.
+//        닉네임 검사 버튼 기능
+        binding.btnIdCheck.setOnClickListener {
+            val inputId = binding.edtNickname.text.toString()
+            ServerUtil.getRequestDuplicatedCheck(
+                "NICK_NAME",
+                inputId,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(jsonObject: JSONObject) {
+                        val code = jsonObject.getInt("code")
+                        runOnUiThread {
+                            when (code) {
+                                200 -> {
+                                    binding.txtIdCheckResult.text = "사용해도 좋은 아이디 입니다."
+                                }
+                                else -> {
+                                    binding.txtIdCheckResult.text = "다른 아이디로 다시 검사해주세요."
+                                }
+                            }
+                        }
+                    }
 
-            binding.txtEmailCheckResult.text = "중복 확인을 해주세요."
-
+                })
         }
+        binding.edtNickname.addTextChangedListener {
+            binding.txtIdCheckResult.text = "중복 확인을 해주세요"
+        }
+
+
 
 
 
@@ -36,28 +59,37 @@ class SignUpActivity : BasicActivity() {
             val inputEmail = binding.edtEmail.text.toString()
 //            서버 중복확인 기능(/user_check - GET) API 활용=> ServerUtil에 함수 추가, 가져다 활용
 //            그 응답 CODE값에 따라 다른 문구 배치.
-            ServerUtil.getRequestDuplicatedCheck("EMAIL",inputEmail, object : ServerUtil.JsonResponseHandler{
-                override fun onResponse(jsonObject: JSONObject) {
+            ServerUtil.getRequestDuplicatedCheck(
+                "EMAIL",
+                inputEmail,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(jsonObject: JSONObject) {
 
 //                    code 값에 따라 이메일 사용 가능 여부.
-                    val code = jsonObject.getInt("code")
-                    runOnUiThread {
-                        when(code){
-                            200 -> {
-                                binding.txtEmailCheckResult.text = "사용해도 좋은 이메일입니다."
-                            }
-                            else -> {
-                                binding.txtEmailCheckResult.text = "다른 이메일로 다시 검사해주세요."
+                        val code = jsonObject.getInt("code")
+                        runOnUiThread {
+                            when (code) {
+                                200 -> {
+                                    binding.txtEmailCheckResult.text = "사용해도 좋은 이메일입니다."
+                                }
+                                else -> {
+                                    binding.txtEmailCheckResult.text = "다른 이메일로 다시 검사해주세요."
+                                }
                             }
                         }
+
                     }
 
-                }
-
-            })
+                })
 
         }
+        binding.edtEmail.addTextChangedListener {
 
+//            내용이 한글자라도 바뀌면, 무조건 재검사 요구 문장.
+
+            binding.txtEmailCheckResult.text = "중복 확인을 해주세요."
+
+        }
 
         binding.btnSignUp.setOnClickListener {
             val inputEmail = binding.edtEmail.text.toString()
@@ -73,20 +105,24 @@ class SignUpActivity : BasicActivity() {
 
 //                        회원가입 성공 /실패 분기
                         val code = jsonObject.getInt("code")
-                        if(code == 200){
+                        if (code == 200) {
 //                            가입한 사람의 닉네임 추출 > ~~님 , 가입을 축하합니다! 토스트
 //                            회원가입화면 종료 > 로그인화면 복귀
                             val dataObj = jsonObject.getJSONObject("data")
                             val userObj = dataObj.getJSONObject("user")
                             val nickname = userObj.getString("nick_name")
                             runOnUiThread {
-                                Toast.makeText(mContext, "${nickname}님 가입을 축하합니다.", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(
+                                    mContext,
+                                    "${nickname}님 가입을 축하합니다.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
-                        }
-                        else{
+                        } else {
                             val message = jsonObject.getString("message")
                             runOnUiThread {
-                                Toast.makeText(mContext, "실패 사유 : ${message}", Toast.LENGTH_SHORT).show()
+                                Toast.makeText(mContext, "실패 사유 : ${message}", Toast.LENGTH_SHORT)
+                                    .show()
                             }
 //                            화면 종료: 객체 소멸(UI 동작 x)
                             finish()
@@ -104,7 +140,6 @@ class SignUpActivity : BasicActivity() {
     override fun setValues() {
 
     }
-
 
 
 }

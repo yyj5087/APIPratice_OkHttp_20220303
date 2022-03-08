71f4e8cc42c7d75277e871d4b9eb8bfd80d28d76
YYJ <naki5087@naver.com>
2022-03-08 16:53:29 +0900

    UserData 클래스 + 추가 ReplyData파싱에 활용
    리스트뷰에 사용할 데이터 클래스 준비 작업

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/datas/ReplyData.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/datas/ReplyData.kt
@@ -7,7 +7,7 @@ class ReplyData(
     var content: String,
 ) {
 
-    val whiter = UserData() //모든 댓글에는 작성자가 있다. null 가능성이 없다
+    var whiter = UserData() //모든 댓글에는 작성자가 있다. null 가능성이 없다
 //    보조 생성자 추가 연습 : 파라미터 X.
     constructor() : this(0,"내용없음")
     companion object{

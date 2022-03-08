9cdac763cb70217d849d6664806d79ce4cb14bc0
YYJ <naki5087@naver.com>
2022-03-08 10:09:54 +0900

    자동로그인 여부 검사에 따른, Intent 대입만 분기 처리

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/SplashActivity.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.nepplus.apipratice_okhttp_20220303
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -17,48 +18,57 @@ class SplashActivity : BasicActivity() {
         setupEvents()
         setValues()
     }
+
     override fun setupEvents() {
 
     }
 
     override fun setValues() {
 
+//        2.5초가 지나기 전에 미리 사용자 정보 조회 시도. (토큰 유효성 검증)
+
+        var isTokenOk = false //검사를 통과하면 true로 변경 예정.
+        ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler {
+            override fun onResponse(jsonObject: JSONObject) {
+
+//                    내 정보를 잘 가져왔다면? => code값을 200으로 내려준다.
+
+//                    200이 아니라면? 무슨 이유던, 사용자 정보 로드 실패. (토큰 유효 X)
+                val code = jsonObject.getInt("code")
+                isTokenOk = (code == 200)
+            }
+
+        })
 //        2.5초만 지나면 -> 자동로그인을 해도 되는지? -> 상황에 맞는 화면으로 이동.
-        
+
         val myHandler = Handler(Looper.getMainLooper())
         myHandler.postDelayed({
-             
+
 //            자동로그인을 해도 되는가?
 //            1) 사용자가 자동로그인 의사를 OK 했는지?
 
             val userAutoLogin = ContextUtil.getAutoLogin(mContext)
 
 //            2) 로그인 시에 받아낸 토큰값이 지금도 유효한지?
-
-            var isTokenOk = false //검사를 통과하면 true로 변경 예정.
-
 //            2-1) 저장된 토큰이 있는지?
 //            2-2) 그 토큰이 사용자 정보를 불러내는지?
-            ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler{
-                override fun onResponse(jsonObject: JSONObject) {
+//            => 2.5초 전에 미리 물어본 상태. isTokenOk에 결과가 들어있다.
 
-//                    내 정보를 잘 가져왔다면? => code값을 200으로 내려준다.
+//           Intent로 화면 이동 => 상황에 따라 다른 Intent를 만든다.
+            val myIntent: Intent
 
-//                    200이 아니라면? 무슨 이유던, 사용자 정보 로드 실패. (토큰 유효 X)
-                    val code = jsonObject.getInt("code")
-                    isTokenOk = (code == 200)
-                }
+            if (userAutoLogin && isTokenOk) {
 
-            })
+//             둘다 ok라면, 바로 메인화면으로
+                myIntent = Intent(mContext, MainActivity::class.java)
+            } else {
+//                아니라면, 로그인 화면으로
+                myIntent = Intent(mContext, LoginActivity::class.java)
+            }
 
-//         if(){
-//
-////             둘다 ok라면, 바로 메인화면으로
-//         }
-//            else{
-////                아니라면, 로그인 화면으로
-//            }
-                              
-        },2500)
+            startActivity(myIntent)
+            finish()
+
+        }, 2500)
     }
 }

ee5fa9d510b463fa41e09e6f00a1b99da3edbb7b
YYJ <naki5087@naver.com>
2022-03-08 10:02:34 +0900

    내 정보를 가져오기를 시도 => 그 결과에 따라 토큰 유효 여부 검증

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/SplashActivity.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/SplashActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.nepplus.apipratice_okhttp_20220303.utils.ContextUtil
+import com.nepplus.apipratice_okhttp_20220303.utils.ServerUtil
+import org.json.JSONObject
 
 class SplashActivity : BasicActivity() {
 
@@ -27,14 +30,34 @@ class SplashActivity : BasicActivity() {
              
 //            자동로그인을 해도 되는가?
 //            1) 사용자가 자동로그인 의사를 OK 했는지?
+
+            val userAutoLogin = ContextUtil.getAutoLogin(mContext)
+
 //            2) 로그인 시에 받아낸 토큰값이 지금도 유효한지?
-         if(){
-             
-//             둘다 ok라면, 바로 메인화면으로
-         }
-            else{
-//                아니라면, 로그인 화면으로
-            }
+
+            var isTokenOk = false //검사를 통과하면 true로 변경 예정.
+
+//            2-1) 저장된 토큰이 있는지?
+//            2-2) 그 토큰이 사용자 정보를 불러내는지?
+            ServerUtil.getRequestMyInfo(mContext, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObject: JSONObject) {
+
+//                    내 정보를 잘 가져왔다면? => code값을 200으로 내려준다.
+
+//                    200이 아니라면? 무슨 이유던, 사용자 정보 로드 실패. (토큰 유효 X)
+                    val code = jsonObject.getInt("code")
+                    isTokenOk = (code == 200)
+                }
+
+            })
+
+//         if(){
+//
+////             둘다 ok라면, 바로 메인화면으로
+//         }
+//            else{
+////                아니라면, 로그인 화면으로
+//            }
                               
         },2500)
     }

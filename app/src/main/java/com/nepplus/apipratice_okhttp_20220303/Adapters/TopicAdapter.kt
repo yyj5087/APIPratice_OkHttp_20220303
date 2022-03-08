36b728e0634743a85f67fed5ac70fb5be6f77637
YYJ <naki5087@naver.com>
2022-03-08 09:44:13 +0900

    참여 인원수 표시 기능

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/Adapters/TopicAdapter.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/Adapters/TopicAdapter.kt
@@ -28,12 +28,14 @@ class TopicAdapter(
         val data = mList[position]
         val txtTile = row.findViewById<TextView>(R.id.txtTitle)
         val imgBackground = row.findViewById<ImageView>(R.id.imgBackground)
+        val txtReplyCount = row.findViewById<TextView>(R.id.txtReplyCount)
         txtTile.text = data.title
 //        data > 서버에서 준 주제 데이터
 //        imageURL 변수 파싱 => 이미지의 인터넷 주소.
 //        웹에 있는 이미지 >이미지뷰에 적용 > Glide 라이브러리
 
         Glide.with(mContext).load(data.imageURL).into(imgBackground)
+        txtReplyCount.text = "${data.replyCount}명 참여중!"
 
         return row
     }

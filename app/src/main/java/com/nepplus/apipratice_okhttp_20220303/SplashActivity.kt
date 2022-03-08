a9fbc5a6debaf930a6ae1bcbb06ac4a000d07228
YYJ <naki5087@naver.com>
2022-03-08 09:47:41 +0900

    로딩화면 추가

---
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.nepplus.apipratice_okhttp_20220303
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BasicActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}

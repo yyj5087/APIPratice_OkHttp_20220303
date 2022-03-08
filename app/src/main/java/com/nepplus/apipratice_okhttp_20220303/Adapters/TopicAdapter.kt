ad1ee0ae26d832ceaafce75e0c6548acda00d1d3
YYJ <naki5087@naver.com>
2022-03-08 09:41:44 +0900

    Glide활용, 토론 주제 이미지 표현

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/Adapters/TopicAdapter.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/Adapters/TopicAdapter.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.nepplus.apipratice_okhttp_20220303.R
 import com.nepplus.apipratice_okhttp_20220303.datas.TopicData
 import org.w3c.dom.Text
@@ -25,7 +27,13 @@ class TopicAdapter(
 
         val data = mList[position]
         val txtTile = row.findViewById<TextView>(R.id.txtTitle)
+        val imgBackground = row.findViewById<ImageView>(R.id.imgBackground)
         txtTile.text = data.title
+//        data > 서버에서 준 주제 데이터
+//        imageURL 변수 파싱 => 이미지의 인터넷 주소.
+//        웹에 있는 이미지 >이미지뷰에 적용 > Glide 라이브러리
+
+        Glide.with(mContext).load(data.imageURL).into(imgBackground)
 
         return row
     }

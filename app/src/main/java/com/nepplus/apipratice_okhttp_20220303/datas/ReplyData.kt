d17ffce94236f122d016d40f1eeed7f32807a934
YYJ <naki5087@naver.com>
2022-03-08 16:59:31 +0900

    만들어져있는 SideData를 활용해서, 댓글의 지지 진영도 파싱

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/datas/ReplyData.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/datas/ReplyData.kt
@@ -8,6 +8,10 @@ class ReplyData(
 ) {
 
     var whiter = UserData() //모든 댓글에는 작성자가 있다. null 가능성이 없다
+
+    var selectedSide = SideData() // 모든 댓글에는 선택한 진영이 있다. null 가능성X
+
+
 //    보조 생성자 추가 연습 : 파라미터 X.
     constructor() : this(0,"내용없음")
     companion object{
@@ -18,6 +22,7 @@ class ReplyData(
             replyData.id = jsonObj.getInt("id")
             replyData.content = jsonObj.getString("content")
             replyData.whiter = UserData.getUserDataFromJson(jsonObj.getJSONObject("user"))
+            replyData.selectedSide = SideData.getSideDataFromJson(jsonObj.getJSONObject("selected_side"))
             return replyData
         }
     }

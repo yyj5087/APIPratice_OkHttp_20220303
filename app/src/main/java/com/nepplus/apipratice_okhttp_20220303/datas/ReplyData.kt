333a72c4151a6f49dc3c358d2360e435ae6e446f
YYJ <naki5087@naver.com>
2022-03-10 09:56:00 +0900

    Calendar의 내부 일시값 변경 연습

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/datas/ReplyData.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/datas/ReplyData.kt
@@ -1,6 +1,7 @@
 package com.nepplus.apipratice_okhttp_20220303.datas
 
 import org.json.JSONObject
+import java.util.*
 
 class ReplyData(
     var id: Int,
@@ -11,6 +12,9 @@ class ReplyData(
 
     var selectedSide = SideData() // 모든 댓글에는 선택한 진영이 있다. null 가능성X
 
+//    작성 일시를 담아둘 변수
+//    일/시 데이터를 변경 => 내부의 숫자만 변경. 변수에 새 객체 대입X => val로 써도 됨
+    val createdAt = Calendar.getInstance()
 
 //    보조 생성자 추가 연습 : 파라미터 X.
     constructor() : this(0,"내용없음")
@@ -23,6 +27,14 @@ class ReplyData(
             replyData.content = jsonObj.getString("content")
             replyData.whiter = UserData.getUserDataFromJson(jsonObj.getJSONObject("user"))
             replyData.selectedSide = SideData.getSideDataFromJson(jsonObj.getJSONObject("selected_side"))
+
+//            Calendar로 되어있는 작성일시의 시간을, 서버가 알려주는 댓글 작성 일시로 맞춰줘야함.
+
+//            임시1) 2022-01-12 10:55:35 로 변경 (한번에 모두 변경)
+            replyData.createdAt.set(2022, 1, 12, 10, 55, 35)
+
+//            임시2) 연도만 2021년으로 변경. (항목을 찍어서 변경)
+            replyData.createdAt.set(Calendar.YEAR, 2021)
             return replyData
         }
     }

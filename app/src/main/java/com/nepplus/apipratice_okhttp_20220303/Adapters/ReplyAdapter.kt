8f813f26bab38e385a46ea071b5af2d15cfd297b
YYJ <naki5087@naver.com>
2022-03-10 10:20:50 +0900

    SimpleData 변경

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/Adapters/ReplyAdapter.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/Adapters/ReplyAdapter.kt
@@ -12,6 +12,8 @@ import com.nepplus.apipratice_okhttp_20220303.R
 import com.nepplus.apipratice_okhttp_20220303.datas.ReplyData
 import com.nepplus.apipratice_okhttp_20220303.datas.TopicData
 import org.w3c.dom.Text
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ReplyAdapter(
     val mContext: Context,
@@ -31,10 +33,22 @@ class ReplyAdapter(
         val txtSelectSide = row.findViewById<TextView>(R.id.txtSelectSide)
         val txtWriterNickname = row.findViewById<TextView>(R.id.txtWriterNickname)
         val txtReplyContent = row.findViewById<TextView>(R.id.txtReplyContent)
+        val txtCreateAt = row.findViewById<TextView>(R.id.txtCreateAt)
 
         txtReplyContent.text = data.content
         txtWriterNickname.text = data.whiter.nickname
         txtSelectSide.text = "${data.selectedSide.title}"
+
+//        임시  -  작성일자만 "2022-03-10" 형태로 표현 => 연, 월, 일 데이터로 가공공
+//        월은 1작게 나옴 +1로 보정
+//        txtCreateAt.text = "${data.createdAt.get(Calendar.YEAR)}-${data.createdAt.get(Calendar.MONTH) + 1}-${data.createdAt.get(Calendar.DAY_OF_MONTH)}"
+
+//        임시2 - "2022-03-10" 형태로 표현 => SImpleDataFormat 활용
+        val sdf =SimpleDateFormat("yyyy-MM-dd")
+
+//        sdf.format(Data객체) => 지정해둔 양식의 String으로 가공
+//        createdAt : Calendar/ format의 피라미터: Data -> calendar 내용물인 time 변수가 Data
+        txtCreateAt.text = sdf.format(data.createdAt.time)
 
 
 

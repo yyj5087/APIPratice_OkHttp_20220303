9f9b0a389a1f0f595870dd5cce590772cf8a9520
win10 <naki5087@naver.com>
2022-03-11 10:52:53 +0900

    let함수 활용

--- app/src/main/java/com/nepplus/apipratice_okhttp_20220303/BasicActivity.kt
+++ app/src/main/java/com/nepplus/apipratice_okhttp_20220303/BasicActivity.kt
@@ -15,9 +15,16 @@ abstract class BasicActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if(supportActionBar != null) {
+
+        supportActionBar?.let {
+
+//            supportActionBar변수가 null이 아닐때만 실행할 코드
             setCustomActionBar()
         }
+
+//        if(supportActionBar != null) {
+//            setCustomActionBar()
+//        }
     }
 
 //    setupEvents / setValues 함수를 만들어두고, 물려주자
